e5ddc866d63c4a0e86e7686341165b915acccccc
alx <37true@gmail.com>
2018-12-03 03:45:54 +0700

    second activity forResult

--- app/src/main/java/tk/alltrue/forresult/MainActivity.java
+++ app/src/main/java/tk/alltrue/forresult/MainActivity.java
@@ -1,10 +1,18 @@
 package tk.alltrue.forresult;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     static final private int CHOOSE_THIEF = 0;
@@ -37,5 +45,34 @@ public class MainActivity extends AppCompatActivity {
                 infoTextView.setText("");
             }
         }
+    }
+
+    public void onClickFeed(View view) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.AppCompatAlertDialogStyle);
+//        builder.setTitle("Dialog");
+//        builder.setMessage("Покормить кота?");
+//        builder.setPositiveButton("OK", null);
+//        builder.setNegativeButton("Cancel", null);
+//        builder.show();
+//        LayoutInflater inflater = getLayoutInflater();
+//        View layout = inflater.inflate(R.layout.custom_layout,
+//                (ViewGroup) findViewById(R.id.toast_layout));
+//
+//        Toast toast = new Toast(getApplicationContext());
+//        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+//        toast.setDuration(Toast.LENGTH_LONG);
+//        toast.setText(R.string.catfood);
+////      toast.setView(layout);
+        Toast toast = Toast.makeText(getApplicationContext(),
+                R.string.catfood, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        ImageView catImageView = new ImageView(getApplicationContext());
+        catImageView.setImageResource(R.drawable.cate1);
+        toastContainer.addView(catImageView, 0);
+//        LinearLayout toastContainer = (LinearLayout) toast.getView();
+//        toastContainer.setBackgroundColor(Color.TRANSPARENT);
+        toast.show();
+
     }
 }
